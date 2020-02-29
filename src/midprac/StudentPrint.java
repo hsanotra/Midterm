bcece81ba4041bc2f335ef17457e75708c4defc6
hsanotra <59582965+hsanotra@users.noreply.github.com>
2020-02-29 11:43:48 -0800

    Update StudentPrint.java

--- src/midprac/StudentPrint.java
+++ src/midprac/StudentPrint.java
@@ -30,6 +30,7 @@ public class StudentPrint
           for(int i=0;i<stu.length;i++)
           {
               System.out.println(stu[i].getName()+" "+stu[i].getId());
+              // System.out.println(stu[i]);
           }       
     }
 }
